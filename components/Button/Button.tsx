31961f61119d5549200af2b31be66e6ecc00bec8
ShiroKatake <tgkhang262@gmail.com>
2025-08-20 00:02:15 +1000

    feat(button): extended to support native anchors and buttons

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,10 +1,17 @@
 import React from 'react';
 import {StyledButton} from './Button.styled';
 
-interface Props {
+interface ButtonProps {
+  url?: string;
+  onClick?: () => void;
   children: React.ReactNode;
 }
 
-export const Button: React.FC<Props> = ({children}) => {
-  return <StyledButton>{children}</StyledButton>;
+export const Button: React.FC<ButtonProps> = ({children, onClick, url}) => {
+  const tag = url ? 'a' : 'button';
+  return (
+    <StyledButton as={tag} onClick={onClick}>
+      {children}
+    </StyledButton>
+  );
 };
