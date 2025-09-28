c1ecf34a0f897abd9853d26114887b44a657c7e5
ShiroKatake <tgkhang262@gmail.com>
2025-09-28 17:32:25 +1000

    feat(image loader): more options from image api

--- lib/contentful/utils.ts
+++ lib/contentful/utils.ts
@@ -19,15 +19,24 @@ export const contentfulLoader = ({
   src,
   width,
   height,
+  quality,
+  fit,
+  focus,
 }: {
   src: string;
   width?: number;
   height?: number;
+  quality?: number;
+  fit?: string;
+  focus?: string;
 }) => {
   const url = new URL(src);
   url.searchParams.set('fm', 'avif');
   if (width) url.searchParams.set('w', width.toString());
   if (height) url.searchParams.set('h', height.toString());
+  if (quality) url.searchParams.set('q', quality.toString());
+  if (fit) url.searchParams.set('fit', fit);
+  if (focus) url.searchParams.set('f', focus);
   return url.href;
 };
 
